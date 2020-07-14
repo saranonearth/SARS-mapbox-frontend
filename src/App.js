0c840ac4a03900ae79f739d79f189d550d170c40
saranonearth <saran.suresh@outlook.com>
2020-07-15 02:01:41 +0530

    Map Base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-function App() {
+const App = () => {
+  const token =
+    "pk.eyJ1Ijoic2FyYW5vbmVhcnRoIiwiYSI6ImNrY21hZndsMDJhc28yc3AwanBmcWVwMDQifQ.DJ1Ba2dZDy-a3HC4ibPRdQ";
+  const [viewport, setViewPort] = useState({
+    width: "100%",
+    height: "100vh",
+    latitude: 10.790483,
+    longitude: 78.704674,
+    zoom: 2,
+  });
+
+  const _onViewportChange = (viewport) =>
+    setViewPort({ ...viewport, transitionDuration: 50 });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapGL
+        {...viewport}
+        mapboxApiAccessToken={token}
+        mapStyle="mapbox://styles/mapbox/dark-v8"
+        onViewportChange={_onViewportChange}
+      ></MapGL>
     </div>
   );
-}
+};
 
 export default App;
